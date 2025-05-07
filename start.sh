af6c887708c1adb51dced2a3d512c4e4d5d312b2
firefart <105281+firefart@users.noreply.github.com>
2025-05-08 00:11:14 +0200

    update

--- start.sh
+++ start.sh
@@ -15,7 +15,6 @@ fi
 docker pull golang:latest
 docker pull alpine:latest
 docker compose pull
-docker compose build --pull
 docker compose stop
 docker compose rm -f -v -s
 docker compose up -d --remove-orphans
