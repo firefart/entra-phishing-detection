7d077543f89484787206335e1578704fc4d6fc5a
Christian Mehlmauer <105281+firefart@users.noreply.github.com>
2025-05-20 13:12:28 +0200

    linting

--- internal/metrics/metrics_test.go
+++ internal/metrics/metrics_test.go
@@ -1,7 +1,6 @@
 package metrics
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -10,18 +9,18 @@ import (
 
 func TestNewMetrics(t *testing.T) {
 	reg := prometheus.NewRegistry()
-	testCounter := prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "test_counter",
+	testMetric := prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "test_total",
 		Help: "A test counter",
 	})
-	err := reg.Register(testCounter)
+	err := reg.Register(testMetric)
 
 	require.NoError(t, err)
 	m, err := NewMetrics(reg)
 	require.NoError(t, err)
 	require.NotNil(t, m)
 	// we need to increment at least once so it will show up in the metrics
-	testCounter.Add(69)
+	testMetric.Add(69)
 	m.ImageHits.WithLabelValues("success").Add(69)
 	gathered, err := reg.Gather()
 	require.NoError(t, err)
@@ -29,22 +28,21 @@ func TestNewMetrics(t *testing.T) {
 	foundCounter := false
 	foundCustom := false
 	for _, metric := range gathered {
-		fmt.Println(metric.GetName())
-		if metric.GetName() == "test_counter" {
+		if metric.GetName() == "test_total" {
 			foundCounter = true
 			require.Len(t, metric.GetMetric(), 1)
-			require.Equal(t, float64(69), metric.GetMetric()[0].GetCounter().GetValue())
+			require.Equal(t, float64(69), metric.GetMetric()[0].GetCounter().GetValue()) // nolint:testifylint
 		}
 		if metric.GetName() == "entra_phishing_detection_image_hits_total" {
 			foundCustom = true
 			require.Len(t, metric.GetMetric(), 1)
 			tmp := metric.GetMetric()[0]
-			require.Equal(t, float64(69), tmp.GetCounter().GetValue())
+			require.Equal(t, float64(69), tmp.GetCounter().GetValue()) // nolint:testifylint
 			labels := tmp.GetLabel()
 			require.Len(t, labels, 1)
 			require.Equal(t, "success", labels[0].GetValue())
 		}
 	}
-	require.True(t, foundCounter, "Expected test_counter to be found in gathered metrics")
+	require.True(t, foundCounter, "Expected test_total to be found in gathered metrics")
 	require.True(t, foundCustom, "Expected entra_phishing_detection_image_hits_total to be found in gathered metrics")
 }
