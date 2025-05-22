e58db669aa031df62dbe8ea63a60b1006c45e825
firefart <105281+firefart@users.noreply.github.com>
2025-05-23 00:07:24 +0200

    recover middleware

---
+++ internal/server/middleware/recover.go
@@ -0,0 +1,20 @@
+package middleware
+
+import (
+	"log/slog"
+	"net/http"
+)
+
+func Recover(logger *slog.Logger) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			defer func() {
+				if err := recover(); err != nil {
+					logger.Error("panic recovered", slog.String("method", r.Method), slog.String("url", r.URL.String()), slog.Any("error", err))
+					http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+				}
+			}()
+			next.ServeHTTP(w, r)
+		})
+	}
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -56,6 +56,7 @@ func NewServer(opts ...OptionsServerFunc) http.Handler {
 		}
 	})
 
+	r.Use(middleware.Recover(s.logger))
 	r.Use(middleware.RealIP(s.config.Server.IPHeader))
 
 	imageRoute := "/image"
